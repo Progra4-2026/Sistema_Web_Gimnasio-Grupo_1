c658fc4b4a6a578861668fb5a175499dee2a3e42
Chris Blanco <chrisblanco.2004@gmail.com>
2026-09-01 20:22:13 -0600

    Estructura inicial: backend Spring Boot + frontend React

---
+++ backend/src/test/java/com/grupo1/gimnasio/gimnasio_backend/GimnasioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1.gimnasio.gimnasio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimnasioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
