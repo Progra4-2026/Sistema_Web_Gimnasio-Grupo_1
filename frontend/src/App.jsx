ee0814c959e8fa4acfdf52bfdd07943e1263a3c4
Chris Blanco <chrisblanco.2004@gmail.com>
2026-09-01 20:19:21 -0600

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+      <div className="app-container">
+        <h1>Sistema Web Gimnasio - Grupo 1</h1>
+        <p>Proyecto de Programación 4</p>
+      </div>
+  )
+}
+
+export default App
